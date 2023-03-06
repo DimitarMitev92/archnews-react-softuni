ee85910ffd41c401157a844ff407096ba1e8bc19
Dimitar Mitev <d.mitev920123@gmail.com>
2023-03-06 13:41:51 +0200

    Update Main.js

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,40 +1,40 @@
-// import { HeroSection } from "./Children/HeroSection.js";
-// import { PartnersSection } from "./Children/PartnersSection.js";
-// import { BlogSection } from "./Children/BlogSection.js";
-// import { AboutUs } from "./Children/AboutUs.js";
+import { HeroSection } from "./Children/HeroSection.js";
+import { PartnersSection } from "./Children/PartnersSection.js";
+import { BlogSection } from "./Children/BlogSection.js";
+import { AboutUs } from "./Children/AboutUs.js";
 
-// import { Login } from "./Children/Login.js";
-// import { Register } from "./Children/Register.js";
-// import { Edit } from "./Children/Edit.js";
-// import { Details } from "./Children/Details.js";
-// import { Create } from "./Children/Create.js";
-// import { ContactUs } from "./Children/ContactUs.js";
-// import { Catalog } from "./Children/Catalog.js";
+import { Login } from "./Children/Login.js";
+import { Register } from "./Children/Register.js";
+import { Edit } from "./Children/Edit.js";
+import { Details } from "./Children/Details.js";
+import { Create } from "./Children/Create.js";
+import { ContactUs } from "./Children/ContactUs.js";
+import { Catalog } from "./Children/Catalog.js";
 
 export const Main = () => {
     return (
         <main>
-            {/* <HeroSection /> */}
+            <HeroSection />
 
-            {/* <PartnersSection /> */}
+            <PartnersSection />
 
-            {/* <BlogSection /> */}
+            <BlogSection />
 
-            {/* <AboutUs /> */}
+            <AboutUs />
 
-            {/* <Login /> */}
+            <Login />
 
-            {/* <Register /> */}
+            <Register />
 
-            {/* <Edit /> */}
+            <Edit />
 
-            {/* <Details /> */}
+            <Details />
 
-            {/* <Create /> */}
+            <Create />
 
-            {/* <ContactUs /> */}
+            <ContactUs />
 
-            {/* <Catalog /> */}
+            <Catalog />
 
         </main>
     );
