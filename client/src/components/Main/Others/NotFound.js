a80036a5329283bff9f5248d716480177e1399f6
Dimitar Mitev <d.mitev920123@gmail.com>
2023-03-14 12:41:49 +0200

    Create more abstract architecture

--- client/src/components/Main/Others/NotFound.js
+++ client/src/components/Main/Others/NotFound.js
@@ -5,7 +5,7 @@ export const NotFound = () => {
         <div className="container d-flex justify-content-center align-items-center text-center" style={{ height: "100vh" }}>
             <div className="row">
                 <div className="col">
-                    <img class="img-fluid" src='https://lh3.googleusercontent.com/pw/AMWts8BWQI_MgwG4BiB3s4Am4TQkP4tfXUUFIQEX0tqVK3CI_9X1meYvJ8zKOVBPaERiH8WLrULxWy7ve4Fbj8wMr3mtjEDlspF2JcYIS1e5Z_xSJJHkg-TRLwmGYYqzepv9Cv9o0qnBH7b_ZYBxVtqSG-tb=w2000-h1000-no?authuser=0' />
+                    <img className="img-fluid" src='https://lh3.googleusercontent.com/pw/AMWts8BWQI_MgwG4BiB3s4Am4TQkP4tfXUUFIQEX0tqVK3CI_9X1meYvJ8zKOVBPaERiH8WLrULxWy7ve4Fbj8wMr3mtjEDlspF2JcYIS1e5Z_xSJJHkg-TRLwmGYYqzepv9Cv9o0qnBH7b_ZYBxVtqSG-tb=w2000-h1000-no?authuser=0' />
                     <h3 className="display-7">THANKS. YOU JUST BROKE IT ALL!</h3>
                     <Link className="btn btn-secondary mt-2" to="/">GET ME OUT OF HERE</Link>
                 </div>
