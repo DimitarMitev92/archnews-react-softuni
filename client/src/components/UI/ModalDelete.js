680c336e257181822632d5589b7e300b4479b411
Dimitar Mitev <d.mitev920123@gmail.com>
2023-04-01 14:04:07 +0300

    Margin Modal Delete

--- client/src/components/UI/ModalDelete.js
+++ client/src/components/UI/ModalDelete.js
@@ -11,7 +11,7 @@ export const ModalDelete = ({
     return (
         <div className="d-flex justify-content-center align-items-center min-vh-100 modal bg-secondary bg-opacity-75">
             <div className="modal-dialog">
-                <div className="modal-content">
+                <div className="modal-content m-3">
                     <div className="modal-header">
                         <h5 className="modal-title">DELETE POST?</h5>
                         <Button
@@ -19,7 +19,7 @@ export const ModalDelete = ({
                             onClick={onClickClose}
                         />
                     </div>
-                    <div className="modal-body">
+                    <div className="modal-body m-3">
                         <p>Are you sure you want to delete this post? If you delete it, you won't be able to recover it.</p>
                     </div>
                     <div className="modal-footer">
