bb293b0f8fe55ea2214e0da2a769a2bacd6b2487
Dimitar Mitev <d.mitev920123@gmail.com>
2023-04-02 15:23:07 +0300

    remove test.js

--- client/src/components/UI/FullScreenImage.js
+++ client/src/components/UI/FullScreenImage.js
@@ -1,5 +1,4 @@
 export const FullScreenImage = ({ imageUrl, onClick }) => {
-    console.log(imageUrl);
     return (
         <div className="d-flex justify-content-center align-items-center min-vh-100 modal bg-secondary bg-opacity-75" onClick={onClick}>
             <img src={imageUrl} className="img-thumbnail w-75 h-75" alt={imageUrl} />
